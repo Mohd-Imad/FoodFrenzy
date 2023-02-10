5005a4155fa9c733a2dee635a2b92bcdf4bbad8f
Mohd-Imad <mohdimd123@gmail.com>
2023-02-10 20:58:39 +0530

    created Repository

---
+++ food-frenzy/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+
+const App = () => {
+  return <>
+    <Login />
+    <Register />
+  </>
+}
+
+export default App
