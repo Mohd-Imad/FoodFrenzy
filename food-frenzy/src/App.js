68989371908eef2f27a7655fe2d973142cbafda4
Mohd-Imad <mohdimd123@gmail.com>
2023-02-11 16:15:37 +0530

    ok

--- food-frenzy/src/App.js
+++ food-frenzy/src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
 import './App.css'
+import ResetPassword from './Components/Login/ResetPassword/ResetPassword'
 
 const App = () => {
   return <>
   <Router>
     <Routes>
       <Route path='/Login' element={<Login />}/>
+      <Route path='/reset' element={<ResetPassword />}/>
       <Route path='/' element={<Register />}/>
     </Routes>
   </Router>
--- food-frenzy/src/Components/Register/Register.jsx
+++ food-frenzy/src/Components/Register/Register.jsx
@@ -5,40 +5,40 @@ import {Link} from 'react-router-dom'
 const Register = () => {
   return <>
     <div className='Register-page-wrapper'>
-      <h1 className='login-heading'>Registration</h1>
-      <div className="form-container">
-        <form className='login-form'>
-          <div className="form-content name-wrapper">
+      <h1 className='register-heading'>Registration</h1>
+      <div className="register-container">
+        <form className='register-form'>
+          <div className="register-content name-wrapper">
             <div className="name-cols">
-              <label className='form-label'>FirstName <span style={{ color: 'red' }}>*</span></label>
-              <input type="text" className='form-input' />
+              <label className='register-label'>FirstName <span style={{ color: 'red' }}>*</span></label>
+              <input type="text" className='register-input' />
             </div>
             <div className="name-cols">
-              <label className='form-label'>LastName (optional)</label>
-              <input type="text" className='form-input' />
+              <label className='register-label'>LastName (optional)</label>
+              <input type="text" className='register-input' />
             </div>
           </div>
-          <div className="form-content">
-            <label className='form-label'>Mobile Number <span style={{ color: 'red' }}>*</span></label>
-            <input type="text" className='form-input' />
+          <div className="register-content">
+            <label className='register-label'>Mobile Number <span style={{ color: 'red' }}>*</span></label>
+            <input type="text" className='register-input' />
           </div>
-          <div className="form-content">
-            <label className='form-label'>Email ID <span style={{ color: 'red' }}>*</span></label>
-            <input type="text" className='form-input' />
+          <div className="register-content">
+            <label className='register-label'>Email ID <span style={{ color: 'red' }}>*</span></label>
+            <input type="text" className='register-input' />
           </div>
-          <div className="form-content">
-            <label className='form-label'>Password <span style={{ color: 'red' }}>*</span></label>
-            <input type="password" className='form-input' />
+          <div className="register-content">
+            <label className='register-label'>Password <span style={{ color: 'red' }}>*</span></label>
+            <input type="password" className='register-input' />
           </div>
-          <div className="form-content">
-            <label className='form-label'>Confirm Password <span style={{ color: 'red' }}>*</span></label>
-            <input type="password" className='form-input' />
+          <div className="register-content">
+            <label className='register-label'>Confirm Password <span style={{ color: 'red' }}>*</span></label>
+            <input type="password" className='register-input' />
           </div>
-          <div className="submit-form-cont">
+          <div className="submit-register-cont">
             <div className="terms-wrapper">
             <input type="checkbox" className='checkbox'/> <span className='terms-msg'>I accept all terms & conditions</span>
             </div>
-            <input type="submit" value='Register' className='login-btn' />
+            <input type="submit" value='Register' className='register-btn' />
             <span className="exist-account-msg">Already have an account? <Link to='/Login' className='navigate'>Login</Link></span>
           </div>
         </form>
