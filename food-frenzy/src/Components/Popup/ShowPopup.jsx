46cfae5b7b225760ba27d908b5638c87a3e34202
Mohd-Imad <mohdimd123@gmail.com>
2023-02-16 23:22:56 +0530

    ok

--- food-frenzy/src/Components/Popup/ShowPopup.jsx
+++ food-frenzy/src/Components/Popup/ShowPopup.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './ShowPopup.css'
+import Login from '../Login/Login'
 
 const ShowPopup = ({ closeModal }) => {
     return <>
-        <div className="modal-wrapper"></div>
-        <div className="modal-container">
-            <h2>My Popup</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique porro labore odio pariatur. Ex vero explicabo saepe cupiditate quia sunt tempore culpa fugiat repellendus sed. Animi rem officia iste quasi!</p>
+        <div className="modal-wrapper">
+            <div className="modal-container">
+                <div className='close'></div>
+                <Login />
+                {/* <h2>My Popup</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique porro labore odio pariatur. Ex vero explicabo saepe cupiditate quia sunt tempore culpa fugiat repellendus sed. Animi rem officia iste quasi!</p>*/}
             <button onClick={closeModal}>Accept</button>
+            </div>
         </div>
     </>
 }
