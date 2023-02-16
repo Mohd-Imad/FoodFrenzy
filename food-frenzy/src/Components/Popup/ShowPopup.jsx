a7484df3cb5320dc5056afb810a93fafc960dc05
Mohd-Imad <mohdimd123@gmail.com>
2023-02-17 00:12:02 +0530

    ok

--- food-frenzy/src/Components/Popup/ShowPopup.jsx
+++ food-frenzy/src/Components/Popup/ShowPopup.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './ShowPopup.css'
 import Login from '../Login/Login'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
 
 const ShowPopup = ({ closeModal }) => {
     return <>
         <div className="modal-wrapper">
             <div className="modal-container">
-                <div className='close'></div>
+                <div className='close'>
+                    <FontAwesomeIcon icon={faClose} />
+                </div>
                 <Login />
                 {/* <h2>My Popup</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique porro labore odio pariatur. Ex vero explicabo saepe cupiditate quia sunt tempore culpa fugiat repellendus sed. Animi rem officia iste quasi!</p>*/}
